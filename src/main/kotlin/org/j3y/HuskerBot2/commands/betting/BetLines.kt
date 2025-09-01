8e7733a3772d304376837ace1dc41c51f338a6e3
JJGerber <jeyrad@gmail.com>
2025-09-01 14:35:58 -0500

    Fixing bet lines command to use cfbd instead of espn

--- src/main/kotlin/org/j3y/HuskerBot2/commands/betting/BetLines.kt
+++ src/main/kotlin/org/j3y/HuskerBot2/commands/betting/BetLines.kt
@@ -2,33 +2,25 @@ package org.j3y.HuskerBot2.commands.betting
 
 import com.fasterxml.jackson.databind.JsonNode
 import net.dv8tion.jda.api.EmbedBuilder
-import net.dv8tion.jda.api.entities.UserSnowflake
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.Command
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import org.j3y.HuskerBot2.commands.SlashCommand
-import org.j3y.HuskerBot2.model.BetEntity
-import org.j3y.HuskerBot2.repository.BetRepo
 import org.j3y.HuskerBot2.repository.ScheduleRepo
-import org.j3y.HuskerBot2.service.EspnService
+import org.j3y.HuskerBot2.service.CfbBettingLinesService
 import org.j3y.HuskerBot2.util.SeasonResolver
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.awt.Color
-import java.time.Instant
-import java.time.LocalDate
-import java.time.temporal.ChronoUnit
 
 @Component
 class BetLines : SlashCommand() {
     val log = LoggerFactory.getLogger(BetLines::class.java)
 
-    final val BIG_10_ID: Int = 5
-
     @Autowired lateinit var scheduleRepo: ScheduleRepo
-    @Autowired lateinit var espnService: EspnService
+    @Autowired lateinit var cfbBettingLinesService: CfbBettingLinesService
 
     override fun getCommandKey(): String = "lines"
     override fun isSubcommand(): Boolean = true
@@ -50,18 +42,16 @@ class BetLines : SlashCommand() {
         val week = commandEvent.getOption("week")?.asInt ?: 1
 
         val sched = scheduleRepo.findBySeasonAndWeek(season, week)
-        val scoreboardData: JsonNode = espnService.getCfbScoreboard(BIG_10_ID, week)
+        val data: JsonNode = cfbBettingLinesService.getLines(season, week, "nebraska")?.path(0)
+            ?: return commandEvent.hook.sendMessage("Unable to find Nebraska Cornhuskers game for week $week.").queue()
 
-        val event: JsonNode = scoreboardData.path("events").find { it.path("name").asText().contains("Nebraska Cornhuskers") } ?:
-            return commandEvent.hook.sendMessage("Unable to find Nebraska Cornhuskers game for week $week.").queue()
+        val lines = data.path("lines").path(0)
 
-        val lines = event.path("competitions").path(0).path("odds").path(0)
-
-        val details = lines.path("details").asText()
+        val details = lines.path("formattedSpread").asText()
         val spread = lines.path("spread").asDouble()
         val overUnder = lines.path("overUnder").asDouble()
 
-        var embed = EmbedBuilder()
+        val embed = EmbedBuilder()
             .setTitle("Opponent Betting Lines")
             .setColor(Color.RED)
             .addField("Opponent", sched?.opponent ?: "N/A", true)
